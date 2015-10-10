0fb008151089dfecff73d091794d5a67e2657ad2
Harvey Xia <harvey@harveyxia.com>
2015-10-10 15:34:18 -0400

    add runner script file

---
+++ scraper/runner.py
@@ -0,0 +1,12 @@
+from scraper import Scraper
+import pickle
+
+scraper = Scraper()
+
+cocktails = scraper.scrape_cocktails()
+cocktails_file = open('cocktails.pickle', 'w')
+pickle.dump(cocktails, cocktails_file)
+
+ingredients = scraper.scrape_ingredients()
+ingredients_file = open('ingredients.pickle', 'w')
+pickle.dump(ingredients, ingredients_file)
