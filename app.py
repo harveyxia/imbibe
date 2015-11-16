1324a6c81e0f58f8bf22587f8102d898514d8fbe
Harvey Xia <harvey@harveyxia.com>
2015-11-16 14:22:41 -0500

    use heroku port

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from psql_client import Client
 from flask import Flask, render_template, request
 app = Flask(__name__)
@@ -22,4 +23,5 @@ def parse_flavor_ids(args):
     return flavor_ids
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
